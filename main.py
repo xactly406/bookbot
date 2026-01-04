52e98a99883242821b60b5a5a434fa8371ca2946
xactly406 <zacktgill@gmail.com>
2026-01-04 11:55:15 -0700

    printed data in readable format

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from stats import word_count, char_count
+from stats import word_count, char_count, sort_dict
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = word_count(text)
     num_chars = char_count(text)
+    sorted_dicts = sort_dict(num_chars)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_chars)
+    print("--------- Character Count -------")
+
+    for dict in sorted_dicts:
+        char = dict["char"]
+        count = dict["num"]
+
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -15,3 +15,20 @@ def char_count(text):
             char_dict[lowered] += 1
 
     return char_dict
+
+def sort_dict(dict):
+
+    sorted_list = []
+
+    #Transform dictionary into a list of dictionaries
+    for char, count in dict.items():
+        if char.isalpha():
+            new_dict = {"char": char, "num": count}
+            sorted_list.append(new_dict)
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
+
+def sort_on(dict):
+    return dict["num"]
